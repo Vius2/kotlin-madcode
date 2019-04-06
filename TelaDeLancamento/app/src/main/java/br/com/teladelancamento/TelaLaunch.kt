dcf3d2f8565131d313a8886025699dc6c9e95661
Vinicius Medeiros <vinicius.medeiros74@gmail.com>
2019-04-06 14:09:34 -0300

    Tela de lançamento

---
+++ TelaDeLancamento/app/src/main/java/br/com/teladelancamento/TelaLaunch.kt
@@ -0,0 +1,36 @@
+package br.com.teladelancamento
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class TelaLaunch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Retirando a barra de status
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(R.layout.activity_tela_launch)
+
+        //Passando para a próxima janela depois de um espaço de tempo
+        Handler().postDelayed({
+
+            val intentParaMain = Intent(this, MainActivity::class.java)
+
+            //Executando o intent
+            startActivity(intentParaMain)
+
+            //Finalizando tela atual
+            finish()
+
+        }, 2000)
+
+    }
+}
